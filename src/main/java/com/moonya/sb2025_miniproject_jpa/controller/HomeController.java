c8045faa11ddf8f0dc77fa6788cff75a9b6b4ecd
Yuk <dbrrmsdn51@naver.com>
2025-03-11 15:17:54 +0900

    feat : thymeleaf 기본문법 예제

--- src/main/java/com/moonya/sb2025_miniproject_jpa/controller/HomeController.java
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/controller/HomeController.java
@@ -1,18 +1,30 @@
 package com.moonya.sb2025_miniproject_jpa.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
-@RequestMapping("/")
 @Log4j2
 public class HomeController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        log.info("model");
+
+        List<String> list = Arrays.asList("캡틴아메리카", "토르", "스파이더맨", "아이언맨");
+
+        model.addAttribute("list", list);
+        model.addAttribute("name", "dooly");
     }
 }
