2aa1ecaab840f94802c6cc267e89ff51652d906b
Yuk <dbrrmsdn51@naver.com>
2025-03-11 11:32:33 +0900

    config : git init

---
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/Sb2025MiniprojectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.moonya.sb2025_miniproject_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2025MiniprojectJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2025MiniprojectJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonya/sb2025_miniproject_jpa/Sb2025MiniprojectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonya.sb2025_miniproject_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2025MiniprojectJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
