20256e93c3ea863b539a77080ea7bf3ffc3661cc
Yuk <dbrrmsdn51@naver.com>
2025-03-12 15:44:54 +0900

    feat : 보드 엔티티 객체 생성

---
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/domain/Board.java
@@ -0,0 +1,32 @@
+package com.moonya.sb2025_miniproject_jpa.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+// 엔티티 클래스에 setter쓰면 죄다 db에 업데이트 날리니까 @쓰지말고 필요한거만 직접 만들자
+@Entity // 아래의 클래스가 db의 테이블로 매핑
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Board extends BaseEntity {
+    @Id // 아래 필드를 pk로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY ) // unique한 값을 생성하는 방법(현재는 mysql의 AI)
+    private Long bno;
+
+    @Column(length = 13, nullable = false)
+    private String writer;
+    @Column(length = 500, nullable = false)
+    private String title;
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
