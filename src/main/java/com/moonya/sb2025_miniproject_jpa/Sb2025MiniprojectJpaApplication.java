83906e84b0411b387a8a1d3843f7723ac8db5994
Yuk <dbrrmsdn51@naver.com>
2025-03-12 15:44:08 +0900

    feat : 리스너 올리기

--- src/main/java/com/moonya/sb2025_miniproject_jpa/Sb2025MiniprojectJpaApplication.java
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/Sb2025MiniprojectJpaApplication.java
@@ -2,8 +2,10 @@ package com.moonya.sb2025_miniproject_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // AuditingEntityListener 리스너 서버 실행 시 자동 등록
 public class Sb2025MiniprojectJpaApplication {
 
 	public static void main(String[] args) {
