999787bbdfefa4be3c2a42af4f0d157323d5998d
Yuk <dbrrmsdn51@naver.com>
2025-03-12 12:37:46 +0900

    feat : 타임리프용법 정리

--- src/main/java/com/moonya/sb2025_miniproject_jpa/controller/HomeController.java
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/controller/HomeController.java
@@ -7,7 +7,23 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Sample {
+    String s1, s2;
+
+    public String getS1() {
+        return this.s1;
+    }
+
+    public String getS2() {
+        return this.s2;
+    }
+}
 
 @Controller
 @Log4j2
@@ -25,6 +41,25 @@ public class HomeController {
         List<String> list = Arrays.asList("캡틴아메리카", "토르", "스파이더맨", "아이언맨");
 
         model.addAttribute("list", list);
-        model.addAttribute("name", "dooly");
+        model.addAttribute("name", "둘리");
+    }
+
+    @GetMapping("/inline")
+    public void inline(Model model) {
+        List<String> strList = IntStream.range(1, 10).mapToObj(i -> "data" + i).collect(Collectors.toList());
+        model.addAttribute("strList", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAA");
+        map.put("B", "BBB");
+        model.addAttribute("map", map);
+
+        model.addAttribute("str", "abc");
+
+        Sample sample = new Sample();
+        sample.s1 = "dooly";
+        sample.s2 = "douner";
+
+        model.addAttribute("obj", sample);
     }
 }
