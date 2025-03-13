19287e66685652cec47ca00b85e624fb6eb31833
Yuk <dbrrmsdn51@naver.com>
2025-03-13 12:48:59 +0900

    test : 페이징까지

--- src/main/java/com/moonya/sb2025_miniproject_jpa/repository/BoardRepository.java
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/repository/BoardRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
     // JpaRepository 에 선언된 메서드들 상속받아서 여쯤 어디에 있는느낌
+
+    //JPA에서는 필요하다면... 쿼리문 이용 가능
 }
--- src/test/java/com/moonya/sb2025_miniproject_jpa/repository/BoardRepositoryTests.java
+++ src/test/java/com/moonya/sb2025_miniproject_jpa/repository/BoardRepositoryTests.java
@@ -5,8 +5,14 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
@@ -44,6 +50,78 @@ public class BoardRepositoryTests {
 
         log.info("select 결과 : " + board);
 
+    }
+
+    @Test
+    public void testSelectList() {
+
+        List<Board> boardList = boardRepository.findAll(Sort.by("bno").descending());
+
+//        for (Board b : boardList){
+//        log.info(b.toString());
+//        }
+
+        boardList.forEach(board -> log.info(board));
+
+    }
+
+    @Test
+    public void testUpdate() {
+        Optional<Board> result = boardRepository.findById(2L);
+        Board board = result.orElseThrow();
+        board.setTitle("수정테스트");
+        board.setContent("테스트트");
+
+        Board oldBoard = Board.builder()
+                .bno(1L)
+                .title("수정")
+                .writer("dooly")
+                .content("저장이 잘 되려나?")
+                .build();
+
+        Board modifiedBoard = boardRepository.save(board);
+        Board modifiedOldBoard = boardRepository.save(oldBoard);
+
+        log.info(modifiedBoard);
+        log.info(modifiedOldBoard);
+    }
+
+    @Test
+    public void testDelete() {
+        Optional<Board> result = boardRepository.findById(3L);
+        if (result.isPresent()){
+        boardRepository.deleteById(3L);
+        }
+    }
+
+//    @Test
+    public void insertDummies() {
+
+        IntStream.range(1,101).forEach(i->{
+            Board newBoard = Board.builder()
+                    .title("title..." + i)
+                    .writer("user" + i % 10)
+                    .content("content..." + i)
+                    .build();
+
+        boardRepository.save(newBoard);
+
+        });
+
+    }
+
+    @Test
+    public void testPaging() {
+        // 페이징 처리는 Pageable객체 파라미터로 전달
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+        result.getContent().forEach(board -> log.info(board));
+
+        if (result.hasNext()) {
+        Page<Board> resultt = boardRepository.findAll(result.getPageable().next());
+        resultt.getContent().forEach(board -> log.info(board));
+        }
     }
 
 }
