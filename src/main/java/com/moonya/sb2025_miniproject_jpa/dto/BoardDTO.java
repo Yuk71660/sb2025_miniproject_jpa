4f3c741ac4fd766d3aaee692d5179c19ac2fd133
Yuk <dbrrmsdn51@naver.com>
2025-03-18 10:10:35 +0900

    feat : 보드의 dto객체 생성

---
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/dto/BoardDTO.java
@@ -0,0 +1,21 @@
+package com.moonya.sb2025_miniproject_jpa.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class BoardDTO {
+    private Long bno;
+
+    private String writer;
+    private String title;
+    private String content;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime modDate;
+}
