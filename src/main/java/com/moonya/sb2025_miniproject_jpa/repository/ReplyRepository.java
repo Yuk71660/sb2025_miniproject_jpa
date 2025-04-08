dfac8b2e45356beab621397ef1567d596a1c5524
Yuk <dbrrmsdn51@naver.com>
2025-04-08 10:17:13 +0900

    feat : 게시글 삭제중 댓글 삭제하기위한 부모글 글번호로 댓글 삭제 메서드 구현

--- src/main/java/com/moonya/sb2025_miniproject_jpa/repository/ReplyRepository.java
+++ src/main/java/com/moonya/sb2025_miniproject_jpa/repository/ReplyRepository.java
@@ -17,4 +17,6 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Query("select r from Reply r where r.board.bno = :bno")
     Page<Reply> replyListOfBoard(@Param("bno") Long bno, Pageable pageable);
 
+    void deleteByBoard_Bno(Long bno);
+
 }
